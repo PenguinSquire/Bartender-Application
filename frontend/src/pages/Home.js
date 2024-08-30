b737369aed777cc98557dd270c68d51432896d15
PenguinSquire <syates03@gmail.com>
2024-08-29 23:27:06 -0400

    adding pages

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+      <div className="center">
+        <h1>Welcome To Our React App!</h1>
+      
+        <p></p>
+      </div>
+    );
+  };
+  
+  export default Home;
